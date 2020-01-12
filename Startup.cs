705a484f5c1bd61280090f62ab5130fafbe93287
aislan peixoto <aislanhouse10@gmail.com>
2020-01-12 13:33:48 -0300

    adicionando versionamento e documentando com swagger

--- Startup.cs
+++ Startup.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Swagger;
 using TalkToApi.DataBase;
+using TalkToApi.Helpers.Swagger;
+using TalkToApi.V1.Models;
+using TalkToApi.V1.Repositories;
+using TalkToApi.V1.Repositories.Contracts;
 
 namespace TalkToApi
 {
@@ -27,11 +40,115 @@ namespace TalkToApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ApiBehaviorOptions>(op =>
+            {
+                op.SuppressModelStateInvalidFilter = true;
+            });
+
+            services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+            services.AddScoped<ITokenRepository, TokenRepository>();
+
             services.AddDbContext<TalkToContext>(cfg =>
             {
                 cfg.UseSqlite("Data Source=Database\\TalkTo.db");
             });
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<TalkToContext>()
+                .AddDefaultTokenProviders();
+
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options => {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("chave-jwt-minhas-tarefas"))
+                };
+            });
+
+            services.AddAuthorization(auth =>
+            {
+                auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+                   .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                   .RequireAuthenticatedUser()
+                   .Build()
+                    );
+            });
+            services.ConfigureApplicationCookie(op =>
+            {
+                op.Events.OnRedirectToLogin = context =>
+                {
+                    context.Response.StatusCode = 401;
+                    return Task.CompletedTask;
+                };
+            });
+
+            services.AddApiVersioning(cfg => {
+                cfg.ReportApiVersions = true;
+                cfg.AssumeDefaultVersionWhenUnspecified = true;
+                cfg.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+            });
+
+            services.AddSwaggerGen(cfg =>
+            {
+                cfg.AddSecurityDefinition("Bearer", new ApiKeyScheme()
+                {
+                    In = "Header",
+                    Type = "apiKey",
+                    Description = "adicione o json web toke para autentica-se",
+                    Name = "Authorization"
+                });
+                var securit = new Dictionary<string, IEnumerable<string>>()
+                {
+                    {"Bearer",new string[]{} }
+                };
+                cfg.AddSecurityRequirement(securit);
+                cfg.ResolveConflictingActions(apidescription => apidescription.First());
+                cfg.SwaggerDoc("v1.0", new Swashbuckle.AspNetCore.Swagger.Info()
+                {
+                    Title = "TalkToApi - v1.0",
+                    Version = "v1.0"
+                });
+
+                var caminhoprojeto = PlatformServices.Default.Application.ApplicationBasePath;
+                var nomeprojeto = $"{PlatformServices.Default.Application.ApplicationName}.xml";
+                var caminhoxmlcomentario = Path.Combine(caminhoprojeto, nomeprojeto);
+                cfg.IncludeXmlComments(caminhoxmlcomentario);
+
+                cfg.DocInclusionPredicate((docName, apiDesc) =>
+                {
+                    var actionApiVersionModel = apiDesc.ActionDescriptor?.GetApiVersion();
+                    // would mean this action is unversioned and should be included everywhere
+                    if (actionApiVersionModel == null)
+                    {
+                        return true;
+                    }
+                    if (actionApiVersionModel.DeclaredApiVersions.Any())
+                    {
+                        return actionApiVersionModel.DeclaredApiVersions.Any(v => $"v{v.ToString()}" == docName);
+                    }
+                    return actionApiVersionModel.ImplementedApiVersions.Any(v => $"v{v.ToString()}" == docName);
+                });
+                cfg.OperationFilter<ApiVersionOperationFilter>();
+
+            });
+
+            services.AddMvc(config => {
+                config.ReturnHttpNotAcceptable = true;
+                config.InputFormatters.Add(new XmlSerializerInputFormatter(config));//xml
+                config.OutputFormatters.Add(new XmlSerializerOutputFormatter());//xml
+            })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(
+                op => op.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +165,12 @@ namespace TalkToApi
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseAuthentication();
+            app.UseSwagger();
+            app.UseSwaggerUI(cfg => {
+                cfg.SwaggerEndpoint("/swagger/v1.0/swagger.json", "TalkToApi - v1.0");
+                cfg.RoutePrefix = string.Empty;
+            });
         }
     }
 }
