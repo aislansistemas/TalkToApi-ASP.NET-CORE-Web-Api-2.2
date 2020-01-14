be59a408ddd3c43531e21332b6ea8f8a452fe0bf
aislan peixoto <aislanhouse10@gmail.com>
2020-01-14 20:30:44 -0300

    criando repositorio de mensagens

---
+++ V1/Repository/MensagemRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TalkToApi.DataBase;
+using TalkToApi.V1.Models;
+using TalkToApi.V1.Repository.Contracts;
+
+namespace TalkToApi.V1.Repository
+{
+    public class MensagemRepository : IMensagemRepository
+    {
+        private readonly TalkToContext _banco;
+        public MensagemRepository(TalkToContext banco)
+        {
+            _banco = banco;
+        }
+        public void Cadastrar(Mensagem mensagem)
+        {
+            _banco.Mensagem.Add(mensagem);
+            _banco.SaveChanges();
+        }
+
+        public List<Mensagem> ObterMensagens(string usuarioUmId, string usuarioDoisId)
+        {
+            return _banco.Mensagem.Where(x => (x.DeId == usuarioUmId || x.DeId == usuarioDoisId) && (x.ParaId == usuarioDoisId || x.ParaId == usuarioDoisId)).ToList();
+        }
+    }
+}
